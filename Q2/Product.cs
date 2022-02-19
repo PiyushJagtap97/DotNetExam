bc8bb3003e2bee88abe4b213a2b79008407a5878
PiyushJagtap97 <93431308+PiyushJagtap97@users.noreply.github.com>
2022-02-19 18:35:18 +0530

    Add files via upload

---
+++ Q2/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppExam.Models
+{
+    public class Product { 
+     public int ProductId { get; set; }
+
+    public String ProductName { get; set; }
+    public Boolean Rate { get; set; }
+    public String Description { get; set; }
+    public int Catogorytype { get; set; }
+}
+}
